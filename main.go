a8457c1cd2fc5f1e47d9bee8074c010c9dc2a0a7
Chandler Miller <chmiller@yescommunities.com>
2023-05-15 11:43:37 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,321 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"snake/path"
+
+	"strconv"
+
+	"time"
+
+	"github.com/gdamore/tcell"
+)
+
+func main() {
+
+	// grid := [][]int{
+	// 	{2, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+	// 	{0, 0, 1, 0, 1, 0, 1, 0, 0, 0},
+	// 	{0, 1, 1, 0, 1, 0, 1, 0, 0, 0},
+	// 	{0, 0, 0, 0, 1, 0, 1, 0, 0, 0},
+	// 	{0, 0, 1, 1, 1, 0, 1, 0, 0, 0},
+	// 	{0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+	// 	{0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
+	// 	{0, 1, 0, 0, 1, 0, 0, 0, 0, 0},
+	// 	{1, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+	// 	{1, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+	// }
+
+	// start := &path.Node{X: 0, Y: 0}
+	// dest := &path.Node{X: 7, Y: 6}
+	// resultPath := path.AStarSearch(start, dest, grid)
+
+	// for _, i := range resultPath {
+	// 	fmt.Println(i.X, i.Y)
+	// }
+
+	// path.PrintPathOnGrid(grid, start, dest, resultPath)
+
+	screen, err := tcell.NewScreen()
+	// width, height := screen.Size()
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	if err := screen.Init(); err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	defStyle := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
+
+	screen.SetStyle(defStyle)
+
+	width, height := screen.Size()
+	grid := make([][]int, width)
+	for i := 0; i < width; i++ {
+		grid[i] = make([]int, height)
+	}
+
+	snakeParts := []SnakePart{
+		{
+			X: 5,
+			Y: 10,
+		},
+		{
+			X: 6,
+			Y: 10,
+		},
+		{
+			X: 7,
+			Y: 10,
+		},
+	}
+
+	snakeBody := SnakeBody{
+		Parts:  snakeParts,
+		Xspeed: 1,
+		Yspeed: 0,
+	}
+
+	game := Game{
+		Screen:    screen,
+		snakeBody: snakeBody,
+		Grid:      grid,
+	}
+
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				screen.Fini() // Clean up the tcell screen before displaying the error
+				fmt.Fprintf(os.Stderr, "Panic occurred: %v\n", r)
+				os.Exit(1)
+			}
+		}()
+
+		go game.Run()
+	}()
+
+	for {
+		switch event := game.Screen.PollEvent().(type) {
+		case *tcell.EventResize:
+			game.Screen.Sync()
+		case *tcell.EventKey:
+			if event.Key() == tcell.KeyEscape || event.Key() == tcell.KeyCtrlC {
+				game.Screen.Fini()
+				os.Exit(0)
+			} else if event.Key() == tcell.KeyUp && game.snakeBody.Yspeed == 0 {
+				game.snakeBody.ChangeDir(-1, 0)
+			} else if event.Key() == tcell.KeyDown && game.snakeBody.Yspeed == 0 {
+				game.snakeBody.ChangeDir(1, 0)
+			} else if event.Key() == tcell.KeyLeft && game.snakeBody.Xspeed == 0 {
+				game.snakeBody.ChangeDir(0, -1)
+			} else if event.Key() == tcell.KeyRight && game.snakeBody.Xspeed == 0 {
+				game.snakeBody.ChangeDir(0, 1)
+			} else if event.Rune() == 'y' && game.GameOver {
+				go func() {
+					defer func() {
+						if r := recover(); r != nil {
+							game.Screen.Fini()
+							fmt.Fprintf(os.Stderr, "Panic occurred: %v\n", r)
+							os.Exit(1)
+						}
+					}()
+
+					game.Run()
+				}()
+			} else if event.Rune() == 'n' && game.GameOver {
+				game.Screen.Fini()
+				os.Exit(0)
+			}
+		}
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+type SnakePart struct {
+	X int
+	Y int
+}
+
+type SnakeBody struct {
+	Parts  []SnakePart
+	Xspeed int
+	Yspeed int
+}
+
+func (sb *SnakeBody) ChangeDir(vertical, horizontal int) {
+	sb.Yspeed = vertical
+	sb.Xspeed = horizontal
+}
+
+func (sb *SnakeBody) Update(width, height int, longerSnake bool) {
+	sb.Parts = append(sb.Parts, sb.Parts[len(sb.Parts)-1].GetUpdatedPart(sb, width, height))
+	if !longerSnake {
+		sb.Parts = sb.Parts[1:]
+	}
+}
+
+func (sb *SnakeBody) ResetPos(width, height int) {
+
+	snakeParts := []SnakePart{
+		{
+			X: int(width / 2),
+			Y: int(height / 2),
+		},
+		{
+			X: int(width/2) + 1,
+			Y: int(height / 2),
+		},
+		{
+			X: int(width/2) + 2,
+			Y: int(height / 2),
+		},
+	}
+
+	sb.Parts = snakeParts
+	sb.Xspeed = 1
+	sb.Yspeed = 0
+}
+
+func (sp *SnakePart) GetUpdatedPart(sb *SnakeBody, width int, height int) SnakePart {
+	newPart := *sp
+	newPart.X = (newPart.X + sb.Xspeed) % width
+
+	if newPart.X < 0 {
+		newPart.X += width
+	}
+
+	newPart.Y = (newPart.Y + sb.Yspeed) % height
+
+	if newPart.Y < 0 {
+		newPart.Y += height
+	}
+
+	return newPart
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+type Game struct {
+	Screen    tcell.Screen
+	snakeBody SnakeBody
+	FoodPos   SnakePart
+	Score     int
+	GameOver  bool
+	Grid      [][]int
+}
+
+func drawParts(s tcell.Screen, snakeParts []SnakePart, foodPos SnakePart, snakeStyle tcell.Style, foodStyle tcell.Style, grid [][]int) (*path.Node, *path.Node, [][]int) {
+
+	s.SetContent(foodPos.X, foodPos.Y, '\u25CF', nil, foodStyle)
+
+	grid[foodPos.X][foodPos.Y] = 3
+
+	for _, part := range snakeParts {
+		s.SetContent(part.X, part.Y, ' ', nil, snakeStyle)
+		grid[part.X][part.Y] = 1
+	}
+
+	grid[snakeParts[0].X][snakeParts[0].Y] = 2
+
+	start := &path.Node{X: snakeParts[0].X, Y: snakeParts[0].Y}
+	dest := &path.Node{X: foodPos.X, Y: foodPos.Y}
+
+	return start, dest, grid
+}
+
+func drawText(s tcell.Screen, x1, y1, x2, y2 int, text string) {
+	row := y1
+	col := x1
+	style := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
+
+	for _, r := range text {
+		s.SetContent(col, row, r, nil, style)
+		col++
+		if col >= x2 {
+			row++
+			col = x1
+		}
+
+		if row > y2 {
+			break
+		}
+	}
+}
+
+func checkCollision(parts []SnakePart, otherPart SnakePart) bool {
+	for _, part := range parts {
+		if part.X == otherPart.X && part.Y == otherPart.Y {
+			return true
+		}
+	}
+	return false
+}
+
+func (g *Game) UpdateFoodPos(width, height int) {
+	g.FoodPos.X = rand.Intn(width)
+	g.FoodPos.Y = rand.Intn(height)
+
+	if g.FoodPos.Y == 1 && g.FoodPos.X < 10 {
+		g.UpdateFoodPos(width, height)
+	}
+}
+
+func (g *Game) Run() {
+	defer func() {
+		if r := recover(); r != nil {
+			g.Screen.Fini()
+			fmt.Fprintf(os.Stderr, "Panic occurred: %v\n", r)
+			os.Exit(1)
+		}
+	}()
+
+	defStyle := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
+	g.Screen.SetStyle(defStyle)
+
+	width, height := g.Screen.Size()
+
+	g.snakeBody.ResetPos(width, height)
+	g.UpdateFoodPos(width, height)
+	g.GameOver = false
+	g.Score = 0
+	snakeStyle := tcell.StyleDefault.Background(tcell.ColorWhite).Foreground(tcell.ColorWhite)
+
+	for {
+		longerSnake := false
+		foodCheck := false
+		g.Screen.Clear()
+		if checkCollision(g.snakeBody.Parts[len(g.snakeBody.Parts)-1:], g.FoodPos) {
+			g.UpdateFoodPos(width, height)
+			longerSnake = true
+			foodCheck = true
+			g.Score++
+		}
+
+		if checkCollision(g.snakeBody.Parts[:len(g.snakeBody.Parts)-1], g.snakeBody.Parts[len(g.snakeBody.Parts)-1]) {
+			break
+		}
+
+		g.snakeBody.Update(width, height, longerSnake)
+		start, dest, grid := drawParts(g.Screen, g.snakeBody.Parts, g.FoodPos, snakeStyle, defStyle, g.Grid)
+		drawText(g.Screen, 1, 1, 8+len(strconv.Itoa(g.Score)), 1, "Score: "+strconv.Itoa(g.Score))
+
+		if foodCheck {
+			newPath := path.AStarSearch(start, dest, grid)
+			if newPath != nil {
+				drawText(g.Screen, width/2-20, height/2, width/2+20, height/2, strconv.Itoa(newPath[0].X)+" "+strconv.Itoa(newPath[0].Y))
+			}
+		}
+
+		time.Sleep(40 * time.Millisecond)
+		g.Screen.Show()
+	}
+
+	g.GameOver = true
+	drawText(g.Screen, width/2-20, height/2, width/2+20, height/2, "Game Over, Score: "+strconv.Itoa(g.Score)+", Play Again? y/n")
+	g.Screen.Show()
+}
---
+++ path/path.go
@@ -0,0 +1,171 @@
+package path
+
+import (
+	"fmt"
+
+	"math"
+)
+
+// Node represents a node in the grid
+type Node struct {
+	X, Y   int
+	G, H   float64
+	Parent *Node
+}
+
+type Coordinate struct {
+	X, Y int
+}
+
+// Updated Node struct to support proper comparison
+func (n *Node) Equal(other *Node) bool {
+	return n.X == other.X && n.Y == other.Y
+}
+
+// A* search algorithm implementation
+func AStarSearch(start, dest *Node, grid [][]int) []*Node {
+	// initialize an open list that only contains the start node.
+	// the open list holds nodes that still need to be checked
+	openList := []*Node{start}
+	// initialize the closed list for nodes that have already been checked
+	closedList := []*Node{}
+
+	for len(openList) > 0 {
+		current := openList[0]
+		currentIndex := 0
+		// Find the node with the lowest total cost (G + H) in the open list
+		for i, node := range openList {
+			if node.G+node.H < current.G+current.H {
+				current = node
+				currentIndex = i
+			}
+		}
+
+		if current.Equal(dest) {
+			// If the current node is the destination node, reconstruct and return the path
+			return reconstructPath(current)
+		}
+
+		// Swap current node with the last node in the open list
+		openList[currentIndex], openList[len(openList)-1] = openList[len(openList)-1], openList[currentIndex]
+		openList = openList[:len(openList)-1]
+		closedList = append(closedList, current)
+
+		// Generate neighboring nodes of the current node
+		neighbors := generateNeighbors(current, grid)
+
+		for _, neighbor := range neighbors {
+			// Skip obstacles and already closed nodes
+			if grid[neighbor.Y][neighbor.X] == 1 || contains(closedList, neighbor) {
+				continue
+			}
+
+			// Calculate the new cost from the start node to the neighbor node
+			newG := current.G + distance(current, neighbor)
+
+			if !contains(openList, neighbor) || newG < neighbor.G {
+				// If the neighbor is not in the open list or the new cost is lower than the previous cost,
+				// update its parent, G score, and H score, and add it to the open list
+				neighbor.Parent = current
+				neighbor.G = newG
+				neighbor.H = distance(neighbor, dest)
+				if !contains(openList, neighbor) {
+					openList = append(openList, neighbor)
+				}
+			}
+		}
+	}
+
+	// No path found
+	return nil
+}
+
+// Reconstruct the path from the destination node to the start node
+
+func reconstructPath(node *Node) []*Node {
+	path := []*Node{node}
+
+	for node.Parent != nil {
+		node = node.Parent
+		path = append([]*Node{node}, path...)
+	}
+
+	return path
+}
+
+// Generate neighboring nodes of a given node
+func generateNeighbors(node *Node, grid [][]int) []*Node {
+	neighbors := []*Node{}
+
+	directions := [][]int{
+		{-1, 0}, // left
+		{1, 0},  // right
+		{0, -1}, // up
+		{0, 1},  // down
+	}
+
+	for _, dir := range directions {
+		x := node.X + dir[0]
+		y := node.Y + dir[1]
+		// Check if the neighbor is within the grid boundaries
+		if x >= 0 && x < len(grid[0]) && y >= 0 && y < len(grid) {
+			neighbors = append(neighbors, &Node{X: x, Y: y})
+		}
+	}
+
+	return neighbors
+}
+
+// Calculate the Euclidean distance between two nodes
+func distance(a, b *Node) float64 {
+	return math.Sqrt(math.Pow(float64(a.X-b.X), 2) + math.Pow(float64(a.Y-b.Y), 2))
+}
+
+// Check if a node is present in a list
+
+func contains(list []*Node, node *Node) bool {
+	for _, n := range list {
+		if n.Equal(node) {
+			return true
+		}
+	}
+	return false
+}
+
+func PrintPathOnGrid(grid [][]int, start, dest *Node, path []*Node) {
+	// Create a copy of the grid to avoid modifying the original grid
+	updatedGrid := make([][]int, len(grid))
+
+	for i := range grid {
+		updatedGrid[i] = make([]int, len(grid[i]))
+		copy(updatedGrid[i], grid[i])
+	}
+
+	// Set the start and destination nodes on the updated grid
+	updatedGrid[start.Y][start.X] = 2
+	updatedGrid[dest.Y][dest.X] = 3
+
+	// Set the path nodes on the updated grid
+	for _, node := range path {
+		updatedGrid[node.Y][node.X] = 4
+	}
+
+	// Print the updated grid
+	for _, row := range updatedGrid {
+		for _, cell := range row {
+			switch cell {
+			case 0:
+				fmt.Print("0 ")
+			case 1:
+				fmt.Print("X ")
+			case 2:
+				fmt.Print("S ")
+			case 3:
+				fmt.Print("D ")
+			case 4:
+				fmt.Print("* ")
+			}
+		}
+		fmt.Println()
+	}
+}
